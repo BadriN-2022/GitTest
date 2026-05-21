31b27e6c19ede6e0103da9b8ba7d6c018106732c
Badri Prasad <badriprasad.n@gmail.com>
2026-05-21 23:55:37 +0530

    app.py added for first time.

---
+++ app.py
@@ -0,0 +1,14 @@
+# sample.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    user_name = "Badri"
+    print(greet(user_name))
+
+    result = add_numbers(10, 20)
+    print("Sum:", result)
